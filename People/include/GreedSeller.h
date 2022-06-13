5f4037da7f20d185e79059a7b40e0669d723a34f
contrub <572519065z@gmail.com>
2022-06-13 20:56:42 +0300

    stealMoney method added + changes in payBill method

--- People/include/GreedSeller.h
+++ People/include/GreedSeller.h
@@ -2,17 +2,18 @@
 #define BUFFET_GREEDSELLER_H
 
 #include "Seller.h"
+#include "Menu.h"
 
 class GreedSeller : public Seller
 {
 public:
     GreedSeller(
             const std::string& first_name, const std::string& second_name, const std::string& last_name,
-            const std::string& organization, const Menu& menu,
-            const double& own_funds, const double& cash_money
+            const std::string& organization, const Menu& menu, const double& own_funds
     );
 
     void payBill(Visitor* visitor) override;
+    double stealMoney(const double& money);
 };
 
 #endif //BUFFET_GREEDSELLER_H
--- People/src/GreedSeller.cpp
+++ People/src/GreedSeller.cpp
@@ -1,13 +1,11 @@
 #include "GreedSeller.h"
-#include "Seller.h"
 
 #include <unistd.h>
 
 GreedSeller::GreedSeller(
         const std::string& first_name, const std::string& second_name, const std::string& last_name,
-        const std::string& organization, const Menu& menu,
-        const double& own_funds, const double& cash_money) :
-        Seller(first_name, second_name, last_name, organization, menu, own_funds, cash_money)
+        const std::string& organization, const Menu& menu, const double& own_funds) :
+        Seller(first_name, second_name, last_name, organization, menu, own_funds)
 {}
 
 void GreedSeller::payBill(Visitor* visitor)
@@ -15,16 +13,22 @@ void GreedSeller::payBill(Visitor* visitor)
     std::string response;
 
     try {
-        if (bills[currentBillIndex]->getTotalPrice() == 0) {
+        if (bills[current_bill - 1]->getTotalPrice() == 0) {
             throw std::invalid_argument("Total bill is empty");
         }
 
-        visitor->spendOwnFunds(bills[currentBillIndex]->getTotalPrice());
-        cash_money += bills[currentBillIndex]->getTotalPrice() * 0.95;
-        own_funds += bills[currentBillIndex]->getTotalPrice() * 0.5;
+        if (bills[current_bill - 1]->getTotalPrice() > visitor->getOwnFunds()) {
+            throw std::invalid_argument("Payment cancelled\nInsufficient funds");
+        }
 
-        std::cout << bills[currentBillIndex]->getTotalPrice();
+        visitor->changeFunds(- bills[current_bill - 1]->getTotalPrice());
 
+        double bill_price = bills[current_bill - 1]->getTotalPrice();
+        double stolen_money = stealMoney(bill_price);
+
+        cash_money += bills[current_bill - 1]->getTotalPrice() - stolen_money;
+
+        bills[current_bill - 1]->changePayStatus();
         response += "Successfully paid!";
     } catch (const std::exception& ex) {
         response = ex.what();
@@ -34,5 +38,13 @@ void GreedSeller::payBill(Visitor* visitor)
               << response << "\n";
 
     std::cout.flush();
-    sleep(2);
+    sleep(3);
+}
+
+double GreedSeller::stealMoney(const double& money)
+{
+    double stolen_money = bills[current_bill - 1]->getTotalPrice() * 0.07;
+    changeFunds(stolen_money);
+
+    return stolen_money;
 }
