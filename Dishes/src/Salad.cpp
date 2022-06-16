8cd881a511825546f66758cb4f5e0365b48150ba
contrub <572519065z@gmail.com>
2022-06-16 09:43:01 +0300

    added default constructors, protected field, overriden method

--- Dishes/src/Salad.cpp
+++ Dishes/src/Salad.cpp
@@ -1,5 +1,16 @@
 #include "Salad.h"
 
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
 Salad::Salad(const std::string& title, const double& price, const double& weight) :
         Dish(title, price, weight)
 {}
+
+std::string Salad::getAmountString() const
+{
+    std::stringstream stream;
+    stream << std::fixed << std::setprecision(2) << (int)getWeight();
+    return stream.str() + " " + weight_units;
+}
