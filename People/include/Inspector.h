2a3ad784802c1cc172f28818a0c63a1dc2c37e9b
contrub <572519065z@gmail.com>
2022-06-16 09:54:26 +0300

    added default constructor, private field, method

--- People/include/Inspector.h
+++ People/include/Inspector.h
@@ -14,10 +14,14 @@ class Inspector : public Human
 public:
     Inspector(
             const std::string& first_name, const std::string& second_name, const std::string& last_name,
-            const std::string& organization
+            const std::string& organization, const double& own_funds
             );
+    Inspector() = default;
 
+    void commitCash(Seller* seller);
     void checkPayment(Seller* seller);
+private:
+    double seller_cash = 0;
 };
 
 
