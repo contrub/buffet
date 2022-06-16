d15ef437f3b6c0540190a19876a01e52d6a8262a
contrub <572519065z@gmail.com>
2022-06-16 09:50:28 +0300

    added default constructor, remove argments from method

--- People/include/GreedSeller.h
+++ People/include/GreedSeller.h
@@ -11,9 +11,10 @@ public:
             const std::string& first_name, const std::string& second_name, const std::string& last_name,
             const std::string& organization, const Menu& menu, const double& own_funds
     );
+    GreedSeller() = default;
 
     void payBill(Visitor* visitor) override;
-    double stealMoney(const double& money);
+    double stealMoney();
 };
 
 #endif //BUFFET_GREEDSELLER_H
